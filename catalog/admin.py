9403231ddf7d4e220d107edfef8c25ffd517f114
samzhuwj <samzhuwj@gmail.com>
2017-11-19 21:48:09 +0800

    register a ModelAdmin class

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,9 +2,30 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language
 
+"""
 # Minimal registration of Models.
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
+"""
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance) 
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
