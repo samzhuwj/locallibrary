db94f5af5e8175c1ca9502d0f8fa529007090d3f
samzhuwj <samzhuwj@gmail.com>
2017-11-19 22:06:52 +0800

    use fieldsets to section the BookInstance detail view

--- catalog/admin.py
+++ catalog/admin.py
@@ -30,3 +30,13 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance) 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+    
