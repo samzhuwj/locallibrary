eeea7797e22fd6b8727e73f4daafa889581a2a59
samzhuwj <samzhuwj@gmail.com>
2017-11-19 18:21:48 +0800

    hook up the url mapper

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
+from django.conf import settings
+# use include() to add urls from the catalog application and authentication system
+from django.conf.urls import url, include
+# use static() to add url mapping to serve static files during development only
+from django.conf.urls.static import static
 from django.contrib import admin
+# use url maps to redirect the base url to our application
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^catalog/', include('catalog.urls')),
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),    
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
