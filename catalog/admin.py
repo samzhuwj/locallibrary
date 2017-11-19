c00d4aa49f19a08eb2704db1f3a66b0655582fc7
samzhuwj <samzhuwj@gmail.com>
2017-11-19 21:50:29 +0800

    configure list views

--- catalog/admin.py
+++ catalog/admin.py
@@ -15,7 +15,7 @@ admin.site.register(Language)
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
 
 admin.site.register(Author, AuthorAdmin)
