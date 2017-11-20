c1bc803e9385294870059b23bbe69b67539b6c2b
samzhuwj <samzhuwj@gmail.com>
2017-11-20 18:37:27 +0800

    use RenewBookModelForm equivalent to RenewBookForm

--- catalog/views.py
+++ catalog/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
 
-from .forms import RenewBookForm
+from .forms import RenewBookModelForm
 from .models import Book, Author, BookInstance, Genre
 
 
@@ -98,12 +98,12 @@ def renew_book_librarian(request, pk):
     if request.method == 'POST':
 
         # Create a form instance and populate it with data from the request (binding):
-        form = RenewBookForm(request.POST)
+        form = RenewBookModelForm(request.POST)
 
         # Check if the form is valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.due_back = form.cleaned_data['due_back']
             book_inst.save()
 
             # redirect to a new URL:
@@ -112,6 +112,6 @@ def renew_book_librarian(request, pk):
     # If this is a GET (or any other method) create the default form
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+        form = RenewBookModelForm(initial={'due_back': proposed_renewal_date,})
 
     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
